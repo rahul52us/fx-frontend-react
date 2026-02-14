04936181606d5d7e828b313fe1ce097e7fa3ba91
Vivek5446 <uknownwarrior04@gmail.com>
2026-02-14 15:43:18 +0530

    mtm table data integration and api

--- src/pages/Dashboard/mtm/component/MTMTable/MTMTable.tsx
+++ src/pages/Dashboard/mtm/component/MTMTable/MTMTable.tsx
@@ -1,50 +1,94 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import CustomTable from "../../../../../config/component/CustomTable/CustomTable";
-import { usePermission } from "../../../../../config/component/customHooks/usePermission";
 import RestrictedAccess from "../../../../../config/component/common/RestrictedAccess/RestrictedAccess";
+import { usePermission } from "../../../../../config/component/customHooks/usePermission";
 
 import store from "../../../../../store/store";
 
 const MTMTable = () => {
   // Permission checks
   const { canView } = usePermission('mtm');
+  
 
-  const BookingRegisterTableColumns = [
-    { headerName: "S.No.", key: "sno", props: { row: { textAlign: "center" } } },
-    { headerName: "Booking Date", key: "bookingDate" },
-    { headerName: "Deal ID", key: "dealId" },
-    { headerName: "Exposure Type", key: "exposureType" },
-    { headerName: "Bank", key: "bank" },
-    { headerName: "Currency", key: "currency" },
-    { headerName: "O/S Amount", key: "osAmount" },
-    { headerName: "Rate Booked", key: "rateBooked" },
-    { headerName: "Delivery Date From", key: "deliveryDateFrom" },
-    { headerName: "Delivery Date To", key: "deliveryDateTo" },
-    { headerName: "Current Fwd. Rate", key: "currentFwdRate" },
-    { headerName: "MTM in INR", key: "mtmInr" },
-    {
-      headerName: "Actions",
-      key: "table-actions",
-      type: "table-actions",
-      props: {
-        row: { minW: 180, textAlign: "center" },
-        column: { textAlign: "center" },
-      },
-    },
-  ];
+  // const BookingRegisterTableColumns = [
+  //   { headerName: "S.No.", key: "sno", props: { row: { textAlign: "center" } } },
+  //   { headerName: "Booking Date", key: "bookingDate" },
+  //   { headerName: "Deal ID", key: "dealId" },
+  //   { headerName: "Exposure Type", key: "exposureType" },
+  //   { headerName: "Bank", key: "bank" },
+  //   { headerName: "Currency", key: "currency" },
+  //   { headerName: "O/S Amount", key: "osAmount" },
+  //   { headerName: "Rate Booked", key: "rateBooked" },
+  //   { headerName: "Delivery Date From", key: "deliveryDateFrom" },
+  //   { headerName: "Delivery Date To", key: "deliveryDateTo" },
+  //   { headerName: "Current Fwd. Rate", key: "currentFwdRate" },
+  //   { headerName: "MTM in INR", key: "mtmInr" },
+  //   {
+  //     headerName: "Actions",
+  //     key: "table-actions",
+  //     type: "table-actions",
+  //     props: {
+  //       row: { minW: 180, textAlign: "center" },
+  //       column: { textAlign: "center" },
+  //     },
+  //   },
+  // ];
+
+const BookingRegisterTableColumns = [
+  { headerName: "S.No.", key: "sno", props: { row: { textAlign: "center" } } },
+
+  { headerName: "Booking Date", key: "bookingDate" },
+
+  { headerName: "Deal Ref No.", key: "hedgeDealReferenceNumber" },
+
+  { headerName: "Exposure Type", key: "exposureType" },
+
+  { headerName: "Bank", key: "bank" },
+
+  { headerName: "Business Unit", key: "bussinessUnit" },
+
+  { headerName: "Currency", key: "currency" },
+
+  { headerName: "O/S Amount", key: "outstandingAmount" },
+
+  { headerName: "Rate Booked", key: "hedgeRate" },
+
+  { headerName: "Delivery Date From", key: "dueDateFrom" },
+
+  { headerName: "Delivery Date To", key: "dueDateTo" },
+
+  { headerName: "Current Fwd. Rate", key: "currentForwardRate" },
+
+  { headerName: "MTM in INR", key: "mtmInInr" },
+
+  { headerName: "Created At", key: "createdAt" },
+
+  // {
+  //   headerName: "Actions",
+  //   key: "table-actions",
+  //   type: "table-actions",
+  //   props: {
+  //     row: { minW: 180, textAlign: "center" },
+  //     column: { textAlign: "center" },
+  //   },
+  // },
+];
+
 
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const rowsPerPage = 10;
   const [data, setData] = useState<any[]>([]);
 
-  const fetchExportRegisterData = async (currentPage = 1) => {
+  const fetchmtmData = async (currentPage = 1) => {
     // setLoading(true);
     try {
       const { viewAsUserId } = store.auth;
       const response = await axios.post(
-        "http://srv864630.hstgr.cloud:8000/mtm/view/",
+        // "http://srv864630.hstgr.cloud:8000/mtm/view/",
+        "http://srv864630.hstgr.cloud:8000/mtmview/view/",
+        
         { condition: "", page: currentPage, limit: rowsPerPage, userId: viewAsUserId }
       );
 
@@ -66,11 +110,11 @@ const MTMTable = () => {
 
   const handlePageChange = (newPage: number) => {
     setPage(newPage);
-    fetchExportRegisterData(newPage);
+    fetchmtmData(newPage);
   };
 
   useEffect(() => {
-    fetchExportRegisterData(page);
+    fetchmtmData(page);
   }, [store.auth.viewAsUserId])
 
   return (
@@ -82,6 +126,10 @@ const MTMTable = () => {
           columns={BookingRegisterTableColumns}
           actions={{
             search: { show: false },
+              resetData: {
+              show: true,
+              function: () => fetchmtmData(1), // Reset to page 1
+            },
             pagination: {
               show: true,
               onClick: handlePageChange,
